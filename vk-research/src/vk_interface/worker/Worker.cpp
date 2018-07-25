92c7bc5b8b3774a1cc5492cd3e54df139400283b
Danylo Rudenko <rudenko.dany@gmail.com>
2018-07-25 11:49:10 +0300

    Worker intermediate.

--- vk-research/src/vk_interface/worker/Worker.cpp
+++ vk-research/src/vk_interface/worker/Worker.cpp
@@ -13,8 +13,15 @@ Worker::Worker(WorkerDesc const& desc)
     : table_{ desc.table_ }
     , device_{ desc.device_ }
     , type_{ desc.type_ }
-    , queue_{ desc.queue_ }
+    , queue_{ VK_NULL_HANDLE }
 {
+    table_->vkGetDeviceQueue(device_->Handle(), desc.queueFamilyIndex_, desc.queueIndex_, &queue_);
+    assert(queue_ != VK_NULL_HANDLE && "Can't get device queue.");
+
+    commandBuffers_.resize(desc.bufferingCount_);
+    for (auto i = 0u; i < desc.bufferingCount_; ++i) {
+        commandBuffers_[i] = VK_NULL_HANDLE;
+    }
 }
 
 Worker::Worker(Worker&& rhs)
